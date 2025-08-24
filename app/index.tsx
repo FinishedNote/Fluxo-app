267380746ed7dc2f89d76d9a47845f7429553b59
Miroir Raphael <darryl.rik@gmail.com>
2025-08-24 14:48:32 +0200

    Add Inter and NovaSquare fonts, update font family in Tailwind config, and declare images

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 flex-center">
-      <Text className="text-3xl text-primary">Fluxo app</Text>
+      <Text className="text-3xl text-primary font-inter font-medium">
+        Fluxo app
+      </Text>
     </View>
   );
 }
