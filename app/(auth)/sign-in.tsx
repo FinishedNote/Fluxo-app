a50fe53e56659e1b4b7a33a5481603156d33105c
Miroir Raphael <darryl.rik@gmail.com>
2025-08-30 18:47:04 +0200

    Enhance SignIn and SignUp components with form handling and validation

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,10 +1,23 @@
+import { Link } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function SignIn() {
   return (
     <View className="flex-1 flex-center bg-background">
-      <Text className="text-white">Sign In</Text>
+      <TextInput className="bg-white p-4 rounded-md" placeholder="Email" />
+      <TextInput
+        className="bg-white p-4 rounded-md"
+        placeholder="Password"
+        secureTextEntry
+      />
+      <Button title="Sign In" onPress={() => {}} />
+      <Text className="text-center text-white mt-4">
+        Don't have an account? Sign Up
+      </Text>
+      <Link href="/sign-up" className="w-full">
+        <Text className="text-center text-white mt-4">Sign Up</Text>
+      </Link>
     </View>
   );
 }
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,10 +1,53 @@
+import { createUser } from "@/lib/appwrite";
+import { router } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function SignUp() {
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+  const [form, setForm] = React.useState({ name: "", email: "", password: "" });
+
+  const handleSignUp = async () => {
+    const { name, email, password } = form;
+
+    if (!name || !email || !password)
+      return alert("Error Please enter valid email address & password.");
+
+    setIsSubmitting(true);
+
+    try {
+      await createUser(email, password, name);
+      router.replace("/");
+    } catch (err: any) {
+      alert(err.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <View>
+    <View className="flex-1 flex-center bg-background">
       <Text>Sign Up</Text>
+      <TextInput
+        className="bg-white p-4 rounded-md"
+        placeholder="Full Name"
+        value={form.name}
+        onChangeText={(text) => setForm({ ...form, name: text })}
+      />
+      <TextInput
+        className="bg-white p-4 rounded-md"
+        placeholder="Email"
+        value={form.email}
+        onChangeText={(text) => setForm({ ...form, email: text })}
+      />
+      <TextInput
+        className="bg-white p-4 rounded-md"
+        placeholder="Password"
+        secureTextEntry
+        value={form.password}
+        onChangeText={(text) => setForm({ ...form, password: text })}
+      />
+      <Button title="Sign Up" onPress={() => handleSignUp()} />
     </View>
   );
 }
